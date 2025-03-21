9f8a77c833a266aebba81299237a75517c2bc7d3
prince-bhalala <princebhalala24@gmail.com>
2025-03-21 23:38:56 +0530

    set up multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import {v2 as cloudinary} from 'cloudinary-core';
+import { log } from 'console';
+import fs from 'fs';
+
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret:process.env.CLOUDINARY_API_SECRET
+});
+
+
+const uplodOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return;
+        //uplod the file in cloudinary 
+        const reponse = await cloudinary.uploader.upload(localFilePath,{
+            resource_type : "auto",
+        })
+        //file has been uploded successfully
+        console.log("File Is Uploded On Cloudinary : ",reponse.url);
+        return reponse ;
+    } catch (error) {
+        fs.unlinkSync(localFilePath); // remoce the localy saved temp file as the uploded got failed
+        return null;
+    }
+};
+
+
+export {uplodOnCloudinary}
