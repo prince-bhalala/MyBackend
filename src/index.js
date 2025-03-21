83ca3bb3b92f7ffa6c511de52ef4fdb919438b6e
prince-bhalala <princebhalala24@gmail.com>
2025-03-21 22:29:39 +0530

    add user and video model

--- src/index.js
+++ src/index.js
@@ -6,4 +6,16 @@ dotenv.config({
 });
 
 
-connectDB();
+connectDB()
+.then( () => {
+    app.listen(process.env.PORT || 3000 , () => {
+        console.log(`Server are Runing at PORT : ${process.env.PORT}`);
+        app.on("error", (error) => {
+            console.log("Error : ", error);
+            throw error;            
+        })
+    })
+})
+.catch((error) => {
+    console.log("MONGO db Connection Failed !!!! ", error)
+})
