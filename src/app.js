019bdb2dd099f383b6cf4807fd0cdd8ce9969944
prince-bhalala <princebhalala24@gmail.com>
2025-03-22 23:04:19 +0530

    write a rout setup

--- src/app.js
+++ src/app.js
@@ -15,4 +15,13 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+// Routes import
+import userRouter from "./routes/user.routes.js";
+
+
+
+
+// Router declaration
+app.use("/api/v1/users",userRouter)
+
 export {app}
