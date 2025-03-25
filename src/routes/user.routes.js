cda98ad5f59fb94c23bf6a9cd6ae8fce6670ba68
prince-bhalala <princebhalala24@gmail.com>
2025-03-25 22:39:20 +0530

    login and logout middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controllers.js";
+import { registerUser ,loginUser,logoutUser} from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -15,5 +16,10 @@ router.route("/register").post(
         }
     ]),
     registerUser)
+
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJWT , logoutUser)
 
 export default router;
