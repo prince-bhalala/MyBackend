2a407a4e24410bca1ec80034909662de9848dc1d
prince-bhalala <princebhalala24@gmail.com>
2025-05-04 00:35:54 +0530

    feat: enhance channel status retrieval with total likes; add health check controller

--- src/controllers/dashboard.controllers.js
+++ src/controllers/dashboard.controllers.js
@@ -7,11 +7,68 @@ import { User } from "../models/user.models.js";
 import { Subscription } from "../models/subscription.models.js";
 import mongoose from "mongoose";
 
-const getChannelStatus = asyncHandler( async (req,res) => {
-  
+const getChannelStatus = asyncHandler(async (req, res) => {
+    const { channelId } = req.params;
 
-    
-})
+    const existChannel = await User.findById(channelId);
+    if (!existChannel) {
+        throw new ApiError(404, "Channel does not exist");
+    }
+
+    const videoStats = await Video.aggregate([
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(channelId)
+            }
+        },
+        {
+            $group: {
+                _id: null,
+                totalViews: { $sum: "$views" },
+                totalVideos: { $sum: 1 }
+            }
+        }
+    ]);
+
+    const totalViews = videoStats[0]?.totalViews || 0;
+    const totalVideos = videoStats[0]?.totalVideos || 0;
+
+    const totalSubscribers = await Subscription.countDocuments({
+        channel: new mongoose.Types.ObjectId(channelId)
+    });
+
+    const likeStats = await Like.aggregate([
+        {
+            $lookup: {
+                from: "videos",
+                localField: "video",
+                foreignField: "_id",
+                as: "videoDetails"
+            }
+        },
+        { $unwind: "$videoDetails" },
+        {
+            $match: {
+                "videoDetails.owner": new mongoose.Types.ObjectId(channelId)
+            }
+        },
+        {
+            $count: "totalLikes"
+        }
+    ]);
+
+    const totalLikes = likeStats[0]?.totalLikes || 0;
+
+    return res.status(200).json(
+        new ApiResponse(200, {
+            totalViews,
+            totalVideos,
+            totalSubscribers,
+            totalLikes
+        }, "Channel stats fetched successfully")
+    );
+});
+
 
 const getChannelVideos = asyncHandler( async (req,res) => {
   
@@ -36,3 +93,7 @@ const getChannelVideos = asyncHandler( async (req,res) => {
     .status(200)
     .json(new ApiResponse(200,video,"All Video fetched Successfully"))
 })
+
+export {    getChannelStatus,
+            getChannelVideos
+}
